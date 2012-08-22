ebe2e45851a10a3775a64e3884d7cdd409c44c91
Vadim <vadim.ivanou@gmail.com>
2012-08-22 10:40:25 -0400

    Added scripts to start and stop SharePoint 2010

---
+++ start-sp.cmd
@@ -0,0 +1,12 @@
+net start MSSQL$SHAREPOINT
+net start SQLWriter
+iisreset /start
+net start SPTimerV4
+net start SPTraceV4
+net start SPAdminV4
+net start SPSearch4
+net start OSearch14
+net start DCLauncher14
+net start DCLoadBalancer14
+net start WebAnalyticsService
+net start SPUserCodeV4net
---
+++ stop-sp.cmd
@@ -0,0 +1,12 @@
+iisreset /stop
+net stop SPTimerV4
+net stop SPTraceV4
+net stop SPAdminV4
+net stop SPSearch4
+net stop OSearch14
+net stop DCLauncher14
+net stop DCLoadBalancer14
+net stop WebAnalyticsService
+net stop SQLWriter
+net stop SPUserCodeV4net
+net stop MSSQL$SHAREPOINT
